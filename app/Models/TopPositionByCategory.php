f0b6e400bf541b63371824de1a16e0817acb6dc8
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2025-08-25 16:07:22 +0300

    Модель TopPositionByCategory

---
+++ app/Models/TopPositionByCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TopPositionByCategory extends Model
+{
+    protected $fillable = [
+        'date',
+        'category_id',
+        'position',
+    ];
+}
